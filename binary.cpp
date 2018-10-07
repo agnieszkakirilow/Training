9466036236afffb9802bc5783866a2efe854132b
Agnieszka <agnieszka.kirilow@gmail.com>
2018-10-07 20:47:29 +0200

    Get highest and lowest bit set is added

--- binary.cpp
+++ binary.cpp
@@ -5,6 +5,8 @@ int getbit(unsigned int number, unsigned int bitnumber);
 void printbinary(unsigned int number);
 void clearbit(unsigned int &number, unsigned int bitnumber);
 void togglebit(unsigned int &number, unsigned int bitnumber);
+int highestsetbit(unsigned int number);
+int lowestsetbit(unsigned int number);
 
 int main()
 {
@@ -12,7 +14,7 @@ int main()
   for(int i = 31; i >=0; i = i-2)
   {
       setbit(number,i);
-  }
+  } 
   for(int i = 31; i >=0; i--)
   {
       std::cout << getbit(number, i);
@@ -27,6 +29,8 @@ int main()
   togglebit(mynumber,4);
   printbinary(mynumber);
   std::cout << std::endl;
+  highestsetbit(15);
+  lowestsetbit(0);
 }
 
 void setbit(unsigned int &number, unsigned int bitnumber)
@@ -55,4 +59,35 @@ void clearbit(unsigned int &number, unsigned int bitnumber)
 void togglebit(unsigned int &number, unsigned int bitnumber)
 {
     number ^= ( 1 << bitnumber );
+}
+
+int highestsetbit(unsigned int number)
+{
+    int bitset = -1;
+    for(int i = 31; i >=0; i--)
+    {
+        if((number >> i) & 1)
+        {
+            bitset = i;
+            break;
+        }
+    }
+    bitset == -1 ? std::cout << "No bit is set\n": std::cout << bitset << " bit is the highest bit set\n";
+    return bitset;
+}
+
+int lowestsetbit(unsigned int number)
+{
+    int bitset = -1;
+    for(int i = 0; i < 32; i++)
+    {
+        if((number >> i) & 1)
+        {
+            bitset = i;
+            break;
+        }
+    }
+    bitset == -1 ? std::cout << "No bit is set\n": std::cout << bitset << " bit is the lowest bit set\n";
+    return bitset;
+    
 }
