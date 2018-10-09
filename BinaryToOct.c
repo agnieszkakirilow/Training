20890afff22bf2c2785bc80ede3b8b780c79d45c
Agnieszka Kirilow <agnieszka.kirilow@gmail.com>
2018-10-09 14:53:46 +0200

    Binary to decimal

--- BinaryToOct.c
+++ BinaryToOct.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 void binaryToOctal(long int number);
+void binaryToDecimal(long int number);
 
 int main()
 {
-  printf("Number:%lo",001111010);
+  printf("Number:%lo",001111010/10);
   binaryToOctal(001111010);
+  binaryToDecimal(001111010);
   return 0;
 }
 
@@ -21,4 +23,18 @@ void binaryToOctal(long int number)
         printf("Remainder:%lo, number:%lo, j:%lo, octal:%lo\n", remainder,number,j,octal);
     }
     printf("Octal: %lo\n", octal);
+}
+
+void binaryToDecimal(long int number)
+{
+    long int decimal = 0;
+    int base = 1, remainder;
+    while (number > 0)
+    {
+        remainder = number % 10;
+        decimal += remainder * base;
+        number /= 10 ;
+        base *= 2;
+    }
+    printf("decimal:%d\n", decimal);
 }
